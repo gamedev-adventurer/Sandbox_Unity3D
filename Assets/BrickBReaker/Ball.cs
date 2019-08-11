dcf257b1441a5c63ac4ca99d18851371fa29dcba
Nightingale Macbook <mar.moreno@gamedevadventurer.com>
2019-08-10 23:38:35 -0500

    basic brick breaker mechanic and delete of vuforia

---
+++ Assets/BrickBReaker/Ball.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 5;
+    private Rigidbody2D rigidBody2D;
+
+    void Awake()
+    {
+
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        rigidBody2D.velocity = Vector2.up * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // Hit the Racket?
+        if (col.gameObject.tag == "racket")
+        {
+            // Calculate hit Factor
+            float x = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.x);
+
+            //float x = col.gameObject.GetComponent<Rigidbody2D>().velocity.x;
+            // Calculate direction, set length to 1
+            Vector2 dir = new Vector2(x, 1).normalized;
+            Debug.Log(dir);
+            // Set Velocity with dir * speed
+            rigidBody2D.velocity = dir * speed;
+        }
+
+        if(col.gameObject.tag == "OutOfBounds")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketWidth)
+    {
+        // ascii art:
+        //
+        // 1  -0.5  0  0.5   1  <- x value
+        // ===================  <- racket
+        //
+        return (ballPos.x - racketPos.x) / racketWidth;
+    }
+}
+
+    
+    
---
+++ Assets/BrickBReaker/Pallet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pallet : MonoBehaviour
+{
+    public float paddleSpeed = 1f;
+
+
+    private Vector3 playerPos,  startingPos;
+
+    private void Start()
+    {
+        playerPos = transform.position;
+        startingPos = playerPos;
+    }
+
+    void Update()
+    {
+        // Get Horizontal Input
+        float h = Input.GetAxisRaw("Horizontal");
+
+        // Set Velocity (movement direction * speed)
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * paddleSpeed;
+
+    }
+}
